392f5ed147d8cb04e87508c5ea550102b40f0900
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2025-07-27 22:25:48 +0530

    Time: 12 ms (29.46%) | Memory: 54.9 MB (99.04%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ //Data ke base pe sort 
+ //BRUTE: EK ARRAYLIST BNA KE THEN REPLACE THE NODES DATA 
+class Solution {
+    public ListNode sortList(ListNode head) 
+    {
+        ArrayList<Integer> arr= new ArrayList<>();
+        ListNode temp= head;
+       
+
+        while(temp!= null)
+        {
+         
+                arr.add(temp.val);
+           
+            temp=temp.next;
+        }
+
+            Collections.sort(arr);
+
+            //again replacing the elements into the nodes
+            int i=0;
+            temp=head;
+            while(temp!=null)
+            {
+                temp.val= arr.get(i);
+                i++;
+                temp=temp.next;
+            }
+
+            return head;
+
+        
+        
+    }
+}
