ac78653270bc77b7d0025924f32066c36f993326
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2025-07-27 18:18:46 +0530

    Time: 3 ms (99.84%) | Memory: 62.8 MB (92.21%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -8,6 +8,9 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ /*
+ //mine: by logic of traversal of fast and slow, removing slow'th element.  done
+ //TC= 0(N/2+N) , SC= 0(1)
 class Solution {
     public ListNode deleteMiddle(ListNode head) 
     {
@@ -15,40 +18,57 @@ class Solution {
         ListNode slow= head;
         ListNode prev= null;
         ListNode temp= head;
-
-
         if(head==null || head.next== null)
         {
             return prev;
         }
-
         while(fast!= null && fast.next!= null)
         {
             slow= slow.next;// agya element to be removed in slow
             fast= fast.next.next;
-            //prev= prev.next;
         }
-
-            //prev.next= slow.next;
-            //prev= slow.next;
-        
+        //removing the slow'th element
         while( temp!= null)
         {
-           
             if(temp.next== slow)
             {
                 temp.next= temp.next.next;
                 break;
-
             }
-            
                 temp=temp.next;
-
         }
-        return head;
-
-
-
-        
+        return head;     
     }
+} //DONE
+*/
+
+//OPTIMAL: [As we see here, that we need the node before the slow ] Thus: Modifying the Hare and Tortoise: skip one traversal of slow.
+class Solution {
+    public ListNode deleteMiddle(ListNode head) 
+    {
+        ListNode fast= head;
+        ListNode slow= head;
+        ListNode res= null;
+       
+       
+        if(head==null || head.next== null)
+        {
+            return res;
+        }
+
+        fast= fast.next.next; //SKIPPED ONE TRAVERSAL OF SLOW 
+        
+        while(fast!= null && fast.next!= null)
+        {
+            slow= slow.next;// agya-> element before the slow
+            fast= fast.next.next; 
+        }
+
+        //now just link exchanging
+        slow.next= slow.next.next;
+       
+    
+    return head;
 }
+}
+
