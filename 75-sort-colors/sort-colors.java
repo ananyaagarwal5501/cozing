449d8d1b138c385a6d6545c65cc3b853b7de7c30
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2025-08-18 22:22:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (30.75%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,4 +1,6 @@
-class Solution {
+//brute: bubble sort simple, done OR DO By Merge Sort
+//TC= 0(N^2): NOT GOOD 
+/*class Solution { 
     public void sortColors(int[] nums) 
     {
         int n= nums.length;
@@ -16,5 +18,48 @@ class Solution {
         }
         }
         
+    }
+}
+*/
+//Better: count all 0, 1 and 2s, then run loop to print all thses
+//TC= 0(N)
+class Solution { 
+    public void sortColors(int[] nums) 
+    {
+        int n= nums.length;
+        int c=0;
+        int v=0;
+        int w=0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]==0)
+            {
+                c++;
+
+            }
+            else if(nums[i]==1)
+            {
+                v++;
+            }
+            else
+            {
+                w++;
+            }
+
+        }
+
+        //printing the array: though 3 loops but TC is 0(N) only
+        for(int i=0; i<c; i++)
+        {
+            nums[i]=0;
+        }
+        for(int i=c; i<v+c; i++)
+        {
+            nums[i]=1;
+        }
+        for(int i=v+c; i<n; i++)
+        {
+            nums[i]=2;
+        }
     }
 }
