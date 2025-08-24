8ad2b18da418bccb338ae4199ad789c258432268
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2025-08-24 13:39:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (20.06%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,5 +1,5 @@
-//by binary: in any 2 power: no of set bits==1 && the most left bit should be 1
-class Solution {
+//by binary: in any 2 power: no of set bits==1 && the most left bit should be 1 &&  n& n-1 is 0.
+/*class Solution {
     public boolean isPowerOfTwo(int n) 
     {
         //Testcases: -ve numbers and zero-> false
@@ -15,16 +15,26 @@ class Solution {
         }
         return (count==1);
     }
-}
+}*/
 
-//approach 2: all n-1 bits should be 0.
+//approach 2: For 2ki poer-> if n& n-1 is 0.
 
-/*class Solution {
+class Solution {
     public boolean isPowerOfTwo(int n) 
     {
        int count=0;
-*/
+       if(n<=0)
+       {
+        return false;
+       }
+      if ((n&(n-1)) ==0)
+      {
+        return true;
+      }
+      return false;
 
+    }
+}
 
 
 
