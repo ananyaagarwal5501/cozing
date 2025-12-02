506393f6d8f2fbd68946181802881f36fdaf2797
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2025-12-02 11:02:37 +0530

    Time: 6 ms (63.76%) | Memory: 47.7 MB (23.92%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,5 +1,40 @@
+//FOR BST: WE KNOW ITS PROPERTY THAT ALL TH ELEFT NODES OF THE ROOT ARE SMALLER THAN IT AND ALL THE RIGHT NODES ARE BIGGER THAN THE ROOT'S VALUE. 
+//ALSO, WKT, FOR THE 2 NODES: 3 COND'S
+                            // 1. EITHER BOTH ON LEFT OF ROOT- root goes left
+                            // 2. EITHER BOTH ON RIGHT OF ROOT- root goes right
+                            // 3. THE NODE FROM WHERE ONE LIES ON LEFT AND THE OTHER ON RIGHT- THE LCA
 
  class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) 
+    {
+        if(root.val> p.val && root.val> q.val)
+        {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        if(root.val< p.val && root.val< q.val)
+        {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+          return root;
+      
+    }   
+ }                                       
+
+
+
+
+
+
+
+
+
+
+
+//follows the BT approach, takes complexity of o(n)
+
+ /*class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) 
     {
         if(root== null || p == root || q == root)
@@ -25,4 +60,4 @@
             return root;
         }
     }
-}
+}*/
