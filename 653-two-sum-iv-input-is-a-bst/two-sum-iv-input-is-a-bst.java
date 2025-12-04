daf021bd5361d372bddad2c9273d931d6f510ab5
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2025-12-04 19:10:00 +0530

    Time: 3 ms (67.95%) | Memory: 47.1 MB (40.90%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -13,26 +13,75 @@
  *     }
  * }
  */
- /*
+
 class Solution {
     public boolean findTarget(TreeNode root, int k) 
     {
-        Stack<TreeNode> stack= new Stack<>();
+        if(root == null)  return false;
+        BTSIterator l= new BTSIterator( root, false); //initialised the next iterator
+        BTSIterator r= new BTSIterator( root, true); //initialised the before iterator
 
-        //next() BSTIterator
-        public findTarget(TreeNode root, int k)
+        int i= l.next();
+        int j= r.next();
+
+        while(i<j)
         {
+            if(i+j == k) return true;
 
+            if(i+j < k) i= l.next();
+            else
+            {
+                j= r.next();
+            }
         }
-        //before() BSTIterator
-
-        //add both to see if we get the key
-
+        return false;
         
     }
 }
 
-*/
+public class BTSIterator
+{
+    Stack<TreeNode> stack= new Stack<>();
+    boolean reverse= true;
+
+    public BTSIterator(TreeNode root, boolean reverse)
+    {
+        this.reverse= reverse;
+        pushAll(root);
+    }
+
+    public void pushAll(TreeNode node)
+    {
+        while( node != null)
+        {
+         stack.push(node); 
+         if(reverse) node= node.right;
+         else
+         {
+            node= node.left;
+         }                       
+        }
+    }
+
+    public boolean hasNext(TreeNode root)
+    {
+        return (!stack.isEmpty());
+    }
+
+    public int next()
+    {
+        TreeNode tmpNode= stack.pop();
+
+        if(reverse) pushAll(tmpNode.left);  //for before one
+        else
+        {
+        pushAll(tmpNode.right); //for next one
+        }
+        return tmpNode.val;  
+    }
+}
+
+ /*
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
         HashSet<Integer> set = new HashSet<>();
@@ -54,3 +103,4 @@ class Solution {
         return dfs(node.left, k, set) || dfs(node.right, k, set);
     }
 }
+*/
