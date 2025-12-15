279946886fc978f732ab8392751b3c5a4aec55b8
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2025-12-15 17:59:32 +0530

    Time: 17 ms (20.87%) | Memory: 48.9 MB (48.74%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,4 +1,78 @@
+//BFS
 class Solution {
+    class Pair {
+    int row, col, step;
+    Pair(int r, int c, int s) {
+        row = r; col = c; step = s;
+    }
+}
+
+    public int[][] updateMatrix(int[][] mat) 
+    {
+        int m= mat.length;
+        int n= mat[0].length;
+
+        Queue<Pair> q= new LinkedList<>();
+        boolean[][] vis= new boolean[m][n]; //0 -> true
+        int[][] dist= new int[m][n];
+        //int step=0;
+
+        //to find the starting points, need to run once thrpugh the given matrix
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(mat[i][j] == 0) //starting points
+                {
+                    q.add(new Pair(i,j, 0)); //1. add to q
+                    vis[i][j] = true; //2. all converted to 0
+                }
+                else
+                {
+                    vis[i][j] = false;
+                }
+            }
+        }
+
+        //bfs
+        while(!q.isEmpty())
+        {
+            int r= q.peek().row;
+            int c= q.peek().col;
+            int step= q.peek().step; ///////////////
+
+            q.remove(); //nikla bahar, pakdo bsd wale ko
+            dist[r][c] = step;//jo bhi unka step tha, wo mark kar diya in the dist matrix
+
+            //ab jaenge padosi ke pas
+            int[] delr= {-1, 0, 1 ,0};
+            int[] delc= {0, 1, 0, -1};
+
+            for(int i=0; i<4; i++)
+            {
+            int nr= r+ delr[i];
+            int nc= c+ delc[i];
+            
+            //checking the padosis and then adding them to q and marking vis.
+            if(nr>=0 && nr < m && nc>=0 && nc< n && vis[nr][nc] != true )
+            {
+            q.add(new Pair(nr, nc, step+1));
+            vis[nr][nc] = true;
+            }
+            }
+        }
+        return dist;
+    }
+    
+}
+
+
+
+
+
+
+
+/*class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
@@ -42,3 +116,4 @@ class Solution {
     }
 }
 
+*/
