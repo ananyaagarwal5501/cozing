2601dc51a3804b4268356f0d633ece1a3a3e1f91
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2025-12-15 20:05:06 +0530

    Time: 2 ms (81.92%) | Memory: 47.6 MB (52.59%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,47 +1,83 @@
+//dfs
 class Solution {
-    public void solve(char[][] board) {
+    public void solve(char[][] board) 
+    {
+        //naap
         int m = board.length;
-        int n = board[0].length;
+        int n= board[0].length;
 
-        // If board too small, nothing to do
-        if (m == 0 || n == 0) return;
+        //the needful
+        int[][] vis= new int[m][n];
 
-        Queue<int[]> queue = new LinkedList<>();
+        int[] dr= {-1, 0, 1, 0};
+        int[] dc= {0, 1, 0, -1};
+        //finding the starting points: found on boundaries, at rows: [0][j], [n-1][j]
+        //a. traversing on boundaries
 
-        // Step 1: Put all border 'O's into queue
-        for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O') queue.add(new int[]{i, 0});
-            if (board[i][n - 1] == 'O') queue.add(new int[]{i, n - 1});
-        }
-        for (int j = 0; j < n; j++) {
-            if (board[0][j] == 'O') queue.add(new int[]{0, j});
-            if (board[m - 1][j] == 'O') queue.add(new int[]{m - 1, j});
-        }
+         //rows:
+          for(int j=0; j<n; j++)
+            {
+                //1st row
+                if(vis[0][j] == 0 && board[0][j]== 'O')
+                {
+                    //agar O aaya-> chack its neighbours now:
+                    dfs(0, j, vis, board, dr, dc);
+                }
+                //last row
+                if(vis[m-1][j] == 0 && board[m-1][j] == 'O')
+                {
+                    dfs(m-1, j, vis, board, dr, dc);
+                }
+            }
+         //cols:
+          for(int i=0; i<m; i++)
+            {
+                //1st col
+                if(vis[i][0] == 0 && board[i][0] == 'O')
+                {
+                    dfs(i, 0, vis, board, dr, dc);
+                }
+                //last col
+                if(vis[i][n-1] == 0 && board[i][n-1] == 'O')
+                {
+                    dfs(i, n-1, vis, board, dr, dc);
+                }
+            }
 
-        // BFS directions
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        // Step 2: BFS from border O's and mark safe region as '#'
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            int r = cell[0], c = cell[1];
-
-            if (r < 0 || c < 0 || r >= m || c >= n || board[r][c] != 'O')
-                continue;
-
-            board[r][c] = '#'; // mark safe
-
-            for (int[] d : dirs) {
-                queue.add(new int[]{r + d[0], c + d[1]});
+        //b. now the whole matrix check for the inside O's
+        for(int i=0; i<m ;i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(board[i][j] == 'O' && vis[i][j] == 0)
+                {
+                    board[i][j] = 'X';
+                }
             }
         }
-
-        // Step 3: convert remaining O -> X, and # -> O
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O') board[i][j] = 'X'; // surrounded ones
-                else if (board[i][j] == '#') board[i][j] = 'O'; // safe ones
-            }
-        }
+       
     }
+        
+        //dfs : jaega neighbours ke pass naa
+        public void dfs(int r, int c, int vis[][], char board[][], int dr[], int dc[])
+        {
+            //pehle mark the aya one bhaii
+            vis[r][c] = 1;
+
+            int m = board.length;
+            int n= board[0].length;
+            //neighbours hai ye
+            for(int i=0; i<4; i++)
+            {
+                int nr= r+ dr[i];
+                int nc= c+ dc[i];
+
+                if(nr>=0 && nr<m && nc>= 0 && nc<n && board[nr][nc]== 'O' && vis[nr][nc] != 1)
+                {
+                    dfs(nr, nc, vis, board, dr, dc);
+                }
+            }
+        }
 }
+            
+     
