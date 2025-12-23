3df73d9578ff70cd6a4f2124f1e10a09c4b2e69b
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2025-12-23 22:25:44 +0530

    Time: 1 ms (62.24%) | Memory: 47.2 MB (43.76%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,4 +1,4 @@
-
+//bfs
 class Solution {
     public boolean isBipartite(int[][] graph) 
     {
@@ -61,43 +61,4 @@ class Solution {
 
 
 
-        //need to take the starting node
-
-        /*class Solution {
-    public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-
-        // color[i] = -1 → not colored
-        // color[i] = 0 or 1 → two different sets
-        int[] color = new int[n];
-        Arrays.fill(color, -1);
-
-        // graph may be disconnected
-        for (int i = 0; i < n; i++) {
-            if (color[i] == -1) {
-                // start BFS
-                Queue<Integer> q = new LinkedList<>();
-                q.add(i);
-                color[i] = 0;
-
-                while (!q.isEmpty()) {
-                    int node = q.poll();
-
-                    for (int nei : graph[node]) {
-                        if (color[nei] == -1) {
-                            // color with opposite color
-                            color[nei] = 1 - color[node];
-                            q.add(nei);
-                        } 
-                        // conflict found
-                        else if (color[nei] == color[node]) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-    }
-}
-*/
+       
