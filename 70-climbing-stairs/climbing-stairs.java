350679be936ebbd6be5d3e50fd18acdce8438f94
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-02-04 22:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (29.76%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,4 +1,27 @@
-class Solution {
+class Solution { //tabulation tc=0(n), sc=0(1)
+    public int climbStairs(int n) 
+    {
+        int[] dp= new int[n+1]; //doesnt signify the number of ways count, but for each step, the computation stored for it
+       Arrays.fill(dp, -1);
+
+        //start from base
+        dp[0]= 1;
+        dp[1]= 1;
+
+        for(int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1]+ dp[i-2];
+        }
+        return dp[n];
+    }
+}
+       
+
+
+
+
+
+/*class Solution { //memoization tc=0(n), sc=0(n)+ aarylist 0(n)
     public int climbStairs(int n) 
     {
         int[] dp= new int[n+1]; //doesnt signify the number of ways count, but for each step, the computation stored for it
@@ -19,4 +42,4 @@ class Solution {
 
            
     }
-}
+}*/
