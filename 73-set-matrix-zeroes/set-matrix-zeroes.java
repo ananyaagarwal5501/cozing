4db84d0bc644931d1024f43553d2f724312bdfe4
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-02-18 23:40:57 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (13.74%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,60 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Step 1: Check first column
+        for(int i = 0; i < n; i++){
+            if(matrix[i][0] == 0){
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Step 2: Check first row
+        for(int j = 0; j < m; j++){
+            if(matrix[0][j] == 0){
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Step 3: Use first row & column as markers
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Step 4: Fill matrix using markers
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 5: Handle first column
+        if(firstColZero){
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        // Step 6: Handle first row
+        if(firstRowZero){
+            for(int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+    }
+}
+
+    
