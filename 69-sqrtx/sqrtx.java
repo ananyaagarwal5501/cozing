f0ec06c8d53d7553c6cfcdf187e042afd109e572
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-03-01 14:50:42 +0530

    Time: 1 ms (97.25%) | Memory: 42.8 MB (22.71%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,59 +1,28 @@
-/*class Solution {
-    public int mySqrt(int x) 
-    {
-        int s= 0;
-        for(int i=1; i<=x; i++)
-        {
-            if(i*i<=x)
-            {
-                s=i;
-            }
-        }
-
-        int p= Math.floor(s);
-        return p;
-    }
-}*/
-//tooo much complexity, goes O(n*n) , thus to reduce the space, we will do by binary serach: optimisation
-
-//Obv.1: the square root of the no. would be definately <=half of that no., thus apply BS
-
 class Solution {
     public int mySqrt(int x) 
     {
-        //edge case
-        if(x<2)
+        //range for the ansewwer, bs will run in the range
+        int l= 1;
+        int e= x;
+
+        while(l<=e)
         {
-            return x;
+            //largest m such that m*m ≤ x
+            int m= l+(e-l)/2;
+            if((long)m*m <= x)
+            {
+                //m could be the answer but we can go more right
+                l= m+1;
+            }
+            else
+            {
+                e= m-1;
+            }
         }
+        //l ends at first false
+        //e ends at last true ✅
+        //return e
 
-        int ans=0;
-        int s=1;
-        int e= x/2;
-
-        while(s<=e)
-        {
-        int m= s+ (e-s)/2;
-
-        long sqrt= (long)m*m;
-
-        if(sqrt==x)
-        {
-            return m;
-        }
-
-        else if(sqrt>x)
-        {
-            e=m-1;
-        }
-        else
-        {
-            s=m+1;
-            ans= m; //prints the last valid result
-
-        }
-        }
-return ans;
+        return e;       
     }
 }
-
