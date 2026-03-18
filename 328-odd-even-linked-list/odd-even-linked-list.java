0d41dcc0de27ac2708ff967f104a398e410992ef
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-03-18 23:16:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (29.60%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -8,73 +8,30 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- 
- class Solution {
+class Solution {
     public ListNode oddEvenList(ListNode head) 
     {
-        ListNode odd= head;
         
+         if(head ==  null || head.next == null) return head;
 
-        if(head== null || head.next== null)
-        {
-            return head;
-        }
+          ListNode odd= head;
+         ListNode even= head.next;
+         ListNode evenHead= even;
 
-        ListNode even= head.next;
-        ListNode evenHead= head.next;
-        while(even!= null && even.next!= null)
-        {
-            odd.next= odd.next.next;
-            even.next= even.next.next;
 
+         while(even != null && even.next!= null)
+         {
+            odd.next= even.next;
             odd= odd.next;
-            even= even.next;
-        }
 
-        odd.next= evenHead;
-    
-    return head;
- }
- }
-
-
-
-
-
-
-        /*ya toh, even walon ko stack meh then odd walon ko then lifo
-        class Solution {
-    public ListNode oddEvenList(ListNode head) 
-    {
-        Stack<Integer> s = new Stack<>();
-         ListNode temp= head;
-         ListNode result= null;
-         int c=0;
-
-         while(temp!= null)
-         {
-            s.push(temp.val);
-            temp=temp.next.next; //odd gye andar
-         }
-
-         temp= head.next;//at index 2
-         while(temp!= null && temp.next!= null)
-         {
-           
-                s.push(temp.val);     
-            temp= temp.next.next;
-
-         }
-
-         
+            even.next= odd.next;
+            even= even.next;            
+         }           
+            odd.next= evenHead;
          
 
-         while(!s.isEmpty())
-         {
-            result.val= s.pop();
-         }
-
-         return result;
+        
+         return head;
+        
     }
-        }
-        */
+}
