e7859f2fe40c983e5376cb1c682d341cd606887c
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-03-18 11:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (97.65%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -8,53 +8,18 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
- /*
- //brute: by stack memory lifo, done
 class Solution {
-    public ListNode reverseList(ListNode head)
-     {
-    ListNode temp= head;
-    Stack<Integer> stack = new Stack<>();
-
-    while(temp!= null)
+    public ListNode reverseList(ListNode head) 
     {
-        stack.push(temp.val);
-        temp= temp.next;
-    }// stack filled
-    
-    temp= head;
-    //fetching from stack and replacing the elements of the LL
-    while(temp!= null)
-    {
-        temp.val= stack.pop();
-        temp= temp.next;
+        ListNode temp =head;
+        ListNode prev= null;
+        while(temp!= null)
+        {
+            ListNode front= temp.next;
+            temp.next= prev;
+            prev= temp;
+            temp= front;
+        }
+        return prev;
     }
-        return head;
-    }
-}*/
-
-
-//optimal: reversal of link of node [cannot use an external data structure], done
-   class Solution {
-    public ListNode reverseList(ListNode head)
-     {
-    ListNode temp= head;
-    ListNode prev= null;
-    
-
-    while(temp!= null)
-    {
-       ListNode front= temp.next;
-        temp.next= prev;
-
-        prev= temp;
-        temp= front;
-    }
-
-    return prev;
-     }
-   }
-
-
-
+}
