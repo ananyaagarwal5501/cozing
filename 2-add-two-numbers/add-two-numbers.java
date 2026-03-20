963ff8dcf596df24d88e1f79c9a47131e36fc926
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-03-20 10:29:30 +0530

    Time: 1 ms (99.89%) | Memory: 46.3 MB (93.92%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -8,66 +8,32 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- //no brute or optimal, as we have to create a new list to store the final nodes
- //THUS, BY DUMMY NODE APPROACH
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
     {
-        ListNode t1= l1;
-        ListNode t2= l2;
-        int carry =0;
-        ListNode dummyNode= new ListNode(-1);
-        ListNode current= dummyNode;//current is the temprory var for the dummynode
+        ListNode dummy= new ListNode(0);
+        ListNode temp = dummy;
+        int carry=0;
 
-        while(t1!= null || t2!= null)
+        while(l1!= null || l2!= null || carry !=0)
         {
             int sum= carry;
-
-            if(t1!= null)
+            if(l1 != null)
             {
-                sum= sum+ t1.val;
+                sum += l1.val;
+                l1= l1.next;
             }
-            if(t2!= null)
+            if(l2 != null)
             {
-                sum= sum+ t2.val;
+                sum += l2.val;
+                l2= l2.next;
             }
-
-            //creating the new node after the sum done and carry is generated
-            ListNode newNode= new ListNode(sum%10);
             carry= sum/10;
 
-            current.next= newNode; // placing the new node after the dummy node
-            current= current.next; // 
-
-
-
-            //updation of t1 and t2
-            if(t1!= null)
-            {
-                t1= t1.next;
-            }
-            if(t2!= null)
-            {
-                t2= t2.next;
-            }
+            temp.next = new ListNode(sum%10);
+            temp= temp.next;
         }
-
-        //checking for the last final carry value 
-        //if carry hai, new node of the value carry
-        if(carry!=0)
-        {
-            ListNode node= new ListNode(carry);
-            current.next= node;//place this node at last
-
-        }
-        return dummyNode.next;
-
-
-
-
-        }
-
-
-    
+        return dummy.next;
         
+    }
 }
