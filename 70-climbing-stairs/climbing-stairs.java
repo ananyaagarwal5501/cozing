40aa3cba0ae3ace4e51c79cb49683df145527c2c
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-02 22:19:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (26.01%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int climbStairs(int n) 
     {
         int c=0;
@@ -17,5 +17,20 @@ class Solution {
             dp[n] = solve(dp, n-1)+ solve(dp, n-2);
         return dp[n];
        
+    }
+}*/
+class Solution {
+    public int climbStairs(int n) 
+    {
+        int prev= 1;
+        int prev1= 1;
+
+        for(int i=2; i<=n; i++)
+        {
+            int curr= prev+prev1;
+            prev1= prev;
+            prev= curr;
+        }
+        return prev;
     }
 }
