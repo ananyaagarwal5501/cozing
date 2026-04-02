07253c4e4eb158bbd5183a36d7c8852594c99960
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-02 22:02:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (10.13%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,47 +1,21 @@
-class Solution { //tabulation tc=0(n), sc=0(1)
+class Solution {
     public int climbStairs(int n) 
     {
-        //int[] dp= new int[n+1]; //doesnt signify the number of ways count, but for each step, the computation stored for it
-       //Arrays.fill(dp, -1);
+        int c=0;
 
-        //start from base
-        int prev= 1;
-        int prev2= 1;
+        int[] dp= new int[n+1];
+        Arrays.fill(dp, -1);
 
-        for(int i=2; i<=n; i++)
+        return solve(dp, n);
+    }
+        public int solve(int[] dp, int n)
         {
-            int curri = prev + prev2;
-            prev2= prev;
-            prev= curri;
-        }
-        return prev;
+            if(n==0) return 1;
+            if(n==1) return 1;
+
+            if(dp[n] != -1) return dp[n];
+            dp[n] = solve(dp, n-1)+ solve(dp, n-2);
+        return dp[n];
+       
     }
 }
-       
-
-
-
-
-
-/*class Solution { //memoization tc=0(n), sc=0(n)+ aarylist 0(n)
-    public int climbStairs(int n) 
-    {
-        int[] dp= new int[n+1]; //doesnt signify the number of ways count, but for each step, the computation stored for it
-       Arrays.fill(dp, -1);
-       return helper(dp, n);
-    }
-
-       int helper(int[] dp, int n)
-       {
-        //edge cases
-        if(n==0) return 1;
-        if(n==1) return 1;
-
-        if(dp[n] != -1) return dp[n];
-
-        dp[n]= helper(dp, n-1) + helper(dp, n-2);
-        return dp[n];
-
-           
-    }
-}*/
