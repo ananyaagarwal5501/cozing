260da0c5765b1ace73e5c913da35aa9d439f0617
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-04 15:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (64.81%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,23 +1,26 @@
 class Solution {
-    
-        int rob(int[] nums) {
-    int n = nums.length;
+    public int rob(int[] nums) 
+    {
+        int n= nums.length;
+        int[] dp= new int[n];
+        Arrays.fill(dp, -1);
 
-    if (n == 1) return nums[0];
-
-    int[] dp = new int[n];
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
-
-    for (int i = 2; i < n; i++) {
-        dp[i] = Math.max(
-            dp[i - 1],
-            nums[i] + dp[i - 2]
-        );
+        return solve( n-1 ,dp, nums);
     }
 
-    return dp[n - 1];
-}
+    int solve(int ind, int[] dp, int[] nums)
+    {
+        //base cases
+        if(ind == 0) return nums[0];
+        if(ind == -1) return 0;
 
+        if(dp[ind] != -1) return dp[ind];
         
+        int pick= nums[ind] + solve(ind-2, dp, nums);
+        int npick= 0 + solve(ind-1, dp, nums);
+
+        dp[ind]= Math.max(pick, npick);
+        
+        return dp[ind];
     }
+}
