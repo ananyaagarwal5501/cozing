0deb884f3a04d5989e4da9079e3341a43e701479
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-04 15:59:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (45.78%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -5,22 +5,27 @@ class Solution {
         int[] dp= new int[n];
         Arrays.fill(dp, -1);
 
-        return solve( n-1 ,dp, nums);
+        return solve( n,n-1 ,dp, nums);
     }
 
-    int solve(int ind, int[] dp, int[] nums)
+    int solve(int n, int ind, int[] dp, int[] nums)
     {
         //base cases
-        if(ind == 0) return nums[0];
-        if(ind == -1) return 0;
+        int prev= nums[0]; //if(ind == 0) return nums[0];
+        int prev2= 0;      //if(ind == -1) return 0;
 
-        if(dp[ind] != -1) return dp[ind];
-        
-        int pick= nums[ind] + solve(ind-2, dp, nums);
-        int npick= 0 + solve(ind-1, dp, nums);
+       // if(dp[ind] != -1) return dp[ind];
+        for(int i=1; i<n; i++)
+        {
+        int pick= nums[i];
+        if(i>1) pick += prev2;
 
-        dp[ind]= Math.max(pick, npick);
-        
-        return dp[ind];
+        int npick= 0 + prev;
+
+        int curri=  Math.max(pick, npick);
+        prev2= prev;
+        prev= curri;
+        }
+        return prev;
     }
 }
