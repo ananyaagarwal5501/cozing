abe66401926bc0f6b4533e61693df3e98bd32a6d
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-09 13:55:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (96.61%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,22 +1,28 @@
 class Solution {
-    int uniquePathsWithObstacles(int[][] grid) {
-    int m = grid.length;
-    int n = grid[0].length;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) 
+    {
+        int[][] grid= obstacleGrid;
+        int m= grid.length;
+        int n= grid[0].length;
 
-    int[] dp = new int[n];
-    dp[0] = grid[0][0] == 1 ? 0 : 1;
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (grid[i][j] == 1) {
-                dp[j] = 0;
-            } else if (j > 0) {
-                dp[j] += dp[j - 1];
-            }
+        int dp[][]= new int[m][n];
+        for(int i=0; i<m; i++)
+        {
+            Arrays.fill(dp[i], -1);
         }
+        return f(grid, m-1, n-1, dp);
     }
 
-    return dp[n - 1];
-}
+    int f(int[][] grid, int i, int j, int[][] dp)
+    {
+        if(i<0 || j<0) return 0;
+        if(grid[i][j] == 1) return 0;
+        if(i==0 && j==0) dp[i][j] =1;
 
+        if(dp[i][j] != -1) return dp[i][j];
+        int left= f(grid, i-1, j, dp);
+        int up= f(grid, i, j-1, dp);
+
+        return dp[i][j]= left+ up;
+    }
 }
