7133d826f1a4c577b41121cf76f507f064e811e8
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-09 14:35:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (70.76%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -6,6 +6,48 @@ class Solution {
         int n= grid[0].length;
 
         int dp[][]= new int[m][n];
+
+        //base case
+        if(grid[0][0] == 1) return 0;
+        //if(i<0 || j<0) return 0;
+        //if(grid[i][j] == 1) dp[i][j] = 0;
+        for(int i=0; i<m; i++) 
+        {
+         if(grid[i][0] == 1) break;
+         dp[i][0] =1;
+        }
+        for(int j=0; j<n; j++)
+        {
+         if(grid[0][j] == 1) break;
+         dp[0][j] =1;
+         
+        }
+
+        for(int i=1; i<m; i++)
+        {
+            for(int j=1; j<n; j++)
+            {
+                if(grid[i][j] ==1) dp[i][j]= 0;
+                else
+                {
+                int left= dp[i][j-1];
+                int up= dp[i-1][j];
+                dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+
+/*class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) 
+    {
+        int[][] grid= obstacleGrid;
+        int m= grid.length;
+        int n= grid[0].length;
+
+        int dp[][]= new int[m][n];
         for(int i=0; i<m; i++)
         {
             Arrays.fill(dp[i], -1);
@@ -25,4 +67,4 @@ class Solution {
 
         return dp[i][j]= left+ up;
     }
-}
+}*/
