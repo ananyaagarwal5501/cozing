9655560dcacc738d11aa19ebf5910f62e2e4b8d5
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-09 21:29:04 +0530

    Time: 1 ms (99.82%) | Memory: 49.8 MB (95.62%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minPathSum(int[][] grid) 
+    {
+        int m= grid.length;
+        int n= grid[0].length;
+        int[][] dp= new int[m][n];
+        for(int i=0; i<m; i++)
+        {
+            Arrays.fill(dp[i], -1);
+        }
+        return f(grid, m-1, n-1, dp);
+    }
+
+    int f(int[][] grid, int i, int j, int[][] dp)
+    {
+       
+        if(i == 0 && j == 0) return grid[0][0];
+        if(i <0 || j<0) return (int)1e9;;
+
+        if(dp[i][j] != -1) return dp[i][j];
+           int left = grid[i][j] + f(grid, i, j-1, dp);
+           int up = grid[i][j] + f(grid, i-1, j, dp);       
+    
+        
+        return dp[i][j]= Math.min(left, up);
+    }
+}
