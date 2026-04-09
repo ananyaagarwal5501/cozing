dac391d7e9388ab7ba71ebd7343969c3f6c667db
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-09 13:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (85.07%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -2,23 +2,20 @@ class Solution {
     public int uniquePaths(int m, int n) 
     {
       int[][] dp= new int[m][n];
-      for(int i=0; i<m; i++)
-      {
-        Arrays.fill(dp[i], -1);
-      }  
-      return f(m-1, n-1, dp); //starting point 
-    }
+      
 
-    int f(int i, int j, int[][] dp)
-    {
-        //base cases
-        if(i == 0 && j == 0) return 1; //reached dest, count path
-        if(i<0 || j<0) return 0; //out of boundary
-
-        if(dp[i][j] != -1) return dp[i][j];
-        int left= f(i, j-1, dp);
-        int up= f(i-1, j, dp);
-
-        return dp[i][j] = left + up;
+        dp[0][0] =1; //if(i == 0 && j == 0) return 1; //reached dest, count path
+        for(int i = 0; i < m; i++) dp[i][0] = 1;
+        for(int j = 0; j < n; j++) dp[0][j] = 1;
+        for(int i=1; i< m; i++)
+        {
+            for(int j=1; j< n; j++)
+            {
+                int up= dp[i-1][j];
+                int left= dp[i][j-1];
+                dp[i][j] = left+ up;
+            }
+        }
+        return dp[m-1][n-1]; //starting point 
     }
 }
