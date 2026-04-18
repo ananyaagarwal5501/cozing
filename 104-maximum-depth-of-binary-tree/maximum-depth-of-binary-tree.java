eaafb7421855cb6996d9055ab2d2a048e1fdb310
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-18 23:43:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (51.08%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -13,6 +13,18 @@
  *     }
  * }
  */
+ class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left= maxDepth(root.left);
+        int right= maxDepth(root.right);
+
+        return 1+  Math.max(left, right);
+    }
+ }
+
+/*
 class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
@@ -33,3 +45,4 @@ class Solution {
         return depth;
     }
 }
+*/
