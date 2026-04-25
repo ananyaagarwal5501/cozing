06935cb0fedba4bb2b9c9c5268d09b1ea24d2728
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-04-26 01:34:08 +0530

    Time: 9 ms (65.36%) | Memory: 43.1 MB (69.36%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,38 +2,41 @@ class Solution {
     public int change(int amount, int[] coins) 
     {
         int n= coins.length;
-        int[][] dp= new int[n][amount+1];
+        int[] prev= new int[amount+1];
+        int[] curr= new int[amount+1];
+        //int[][] dp= new int[n][amount+1];
         
         //base case
         if(amount == 0) 
         {
           for(int i=0; i<n; i++)
           {
-            dp[i][0] = 1;
+            prev[0] = 1;
           }
         }
          for(int i=0; i<=amount; i++)
             {
-                if(i % coins[0] == 0) dp[0][i] = 1;
+                if(i % coins[0] == 0) prev[i] = 1;
                 else
-                dp[0][i] = 0;
+                prev[i] = 0;
             }
         for(int ind=1; ind<n; ind++)
         {
             for(int tar=0; tar<= amount; tar++)
             {
-                int ntake= dp[ind-1][tar];
+                int ntake= prev[tar];
                 int take=0;
                 if(coins[ind]<= tar)
                 {
-                 take= dp[ind][tar- coins[ind]];
+                 take= curr[tar- coins[ind]];
                 }
-                dp[ind][tar]= take + ntake;
+                curr[tar]= take + ntake;
             }
+            prev= curr;
         }
         
 
-        return dp[n-1][amount];
+        return prev[amount];
 
 
     }
