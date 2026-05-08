fe17b380893c18cbf57604b3be42c6501ec1becd
ananyaagarwal5501 <153985981+ananyaagarwal5501@users.noreply.github.com>
2026-05-08 21:36:07 +0530

    Time: 4 ms (48.44%) | Memory: 44.7 MB (46.29%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,71 +1,33 @@
 class Solution {
-    public boolean isPalindrome(String s)
-     {
+    public boolean isPalindrome(String s) 
+    {
         int n= s.length();
-        s= s.toLowerCase();
-        StringBuilder sb= new StringBuilder(s); 
-        
-
-        StringBuilder sb2= new StringBuilder();
-
-        // lowercase //removing all non-alphanumeric //same forward and backward
-
+        StringBuilder sb= new StringBuilder();
         for(int i=0; i<n; i++)
         {
-            char ch=sb.charAt(i);
-            if((sb.charAt(i)>='a' && sb.charAt(i)<='z') ||(sb.charAt(i)>='0' && sb.charAt(i)<='9') )
+            char ch= s.charAt(i);
+            if(Character.isLetterOrDigit(ch))
             {
-                sb2.append(ch);
+                sb.append(Character.toLowerCase(ch));
             }
         }
-        StringBuilder sb3= new StringBuilder(sb2).reverse(); //correct way to store the reversed stringbuilder
-        //sb3= sb2.reverse();///bcccccc, here i am changing the original value of sb2 also!!! tabhi wrong
 
+            String a= sb.toString();
+            int l=a.length();  
+            int st=0, e=l-1;
 
-        //NO NEED OF SUCH BIF STATEMENTS, INSTEAD WRITE..line
-        /*if((sb2.toString()).equals(sb3.toString()))
-        {
-            return true;
-        }
-
-        else
-       {
-        return false;
-       }*/
-
-       return (sb2.toString()).equals(sb3.toString());
-
-        
-
-
-
-        /*int z=sb2.length();
-        int st=0;
-        int e= z-1;
-        while(st<=e)
-        {
-            if(sb2.charAt(st)==sb2.charAt(e))
+            Boolean ans= true;
+            while(st<e)
             {
+                if(a.charAt(st) != a.charAt(e))
+                {
+                    ans= false;
+                    break;
+                }
                 st++;
                 e--;
             }
-        }
-        boolean h=true;
-        if(st==e)
-        {
-            h= true;
-
-        }
-        else 
-        {
-            h=false;
-        }
-        */
-        
-        //return h;
-        
-
-
+        return ans;
         
     }
 }
